5763bf7fae2e2fc2133a6584e4c4494cb12bf86f
DameHetfield9803 <you@example.com>
2023-12-22 22:55:15 +0800

    bla bla bla

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+const names = ref([{ name: "Damien", age: 20 }, { name: "En Quan", age: 20 }, { name: "Daniel", age: 21 }, { name: "Firdaus", age: 22 }])
+</script>
+
+<template>
+  <div class="one">
+    <h1>
+      <ul>
+        <li v-for="n in names">Hello {{ n.name }},
+          you are {{ n.age }} years old </li>
+      </ul>
+    </h1>
+  </div>
+</template>
+
+<style scoped></style>
